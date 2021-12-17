5d9575e268f291693ce1262694136b719676ca65
loichyan <loichyan@foxmail.com>
2021-12-17 18:44:17 +0800

    feat(builtin): add `builtin.tree.filters`

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -6,14 +6,25 @@ function M.preset()
     keymaps = {
       toggle = "<C-n>",
     },
+    filters = { [".git"] = true, [".cache"] = true, ["node_modules"] = true },
   }
   return preset
 end
 
 ---@param preset MiNVPresetTree
 function M.setup(preset, tree)
-  tree.setup({})
   local utils = require("utils")
+  tree.setup({
+    filters = {
+      dotfiles = false,
+      custom = utils.set_to_list(preset.filters),
+    },
+    git = {
+      enable = true,
+      ignore = true,
+      timeout = 500,
+    },
+  })
   local keymaps = preset.keymaps
   utils.map("n", keymaps.toggle, ":NvimTreeToggle<CR>")
 end
