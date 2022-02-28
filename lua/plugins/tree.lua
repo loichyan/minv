fb383eac81bee5a30262e23c74663a095d9726bf
loichyan <loichyan@foxmail.com>
2022-02-28 09:08:49 +0800

    feat(buitlin): update setup options of `nvim-tree`

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -3,11 +3,38 @@ local M = {}
 function M.preset()
   ---@class MiNVPresetTree
   local preset = {
+    setup = {
+      disable_netrw = true,
+      auto_close = true,
+      update_cwd = true,
+      update_to_buf_dir = {
+        enable = true,
+        auto_open = true,
+      },
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      git = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
+      },
+      view = {
+        auto_resize = true,
+      },
+      filters = {
+        custom = {
+          ".git",
+          ".cache",
+          "node_modules",
+        },
+      },
+    },
     keymaps = {
       toggle = "<C-b>",
       focus = "<C-n>",
     },
-    filters = { [".git"] = true, [".cache"] = true, ["node_modules"] = true },
   }
   return preset
 end
@@ -17,17 +44,10 @@ function M.setup(preset)
   local tree = require("nvim-tree")
   local utils = require("utils")
 
-  tree.setup({
-    filters = {
-      dotfiles = false,
-      custom = utils.set_to_list(preset.filters),
-    },
-    git = {
-      enable = true,
-      ignore = false,
-      timeout = 500,
-    },
-  })
+  -- Setup nvim-tree.
+  tree.setup(preset.setup)
+
+  -- Set keymaps.
   local keymaps = preset.keymaps
   utils.keymaps({
     { keymaps.toggle, "<Cmd>NvimTreeToggle<CR>" },
