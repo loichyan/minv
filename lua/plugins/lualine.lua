fdc1c672970d344c2676c76a7f41215f58314eca
loichyan <loichyan@foxmail.com>
2022-02-28 19:33:09 +0800

    feat(builtin): update setup options of `lualine`

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,55 @@
 local M = {}
 
+function M.components()
+  return {
+    mode = {
+      function()
+        return " "
+      end,
+      padding = { left = 1, right = 1 },
+    },
+    branch = { "branch" },
+    filename = { "filename" },
+    diff = { "diff" },
+    diagnostics = { "diagnostics", sources = { "nvim_diagnostic" } },
+    encoding = { "encoding", fmt = string.upper },
+    fileformat = { "fileformat" },
+    filetype = { "filetype", fmt = string.upper },
+    progress = { "progress" },
+  }
+end
+
 function M.preset()
+  local components = M.components()
   ---@class MiNVPresetLualine
   local preset = {
     setup = {
       options = {
         theme = "auto",
+      },
+      sections = {
+        lualine_a = {
+          components.mode,
+        },
+        lualine_b = {
+          components.branch,
+          components.filename,
+        },
+        lualine_c = {
+          components.diff,
+        },
+        lualine_x = {
+          components.diagnostics,
+          components.lsp,
+        },
+        lualine_y = {
+          components.encoding,
+          components.fileformat,
+          components.filetype,
+        },
+        lualine_z = {
+          components.progress,
+        },
       },
     },
     extensions = {
