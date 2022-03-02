d759271329926c8780227ecbf530f28bc02e5863
loichyan <loichyan@foxmail.com>
2022-03-02 16:08:58 +0800

    feat(builtin): update setup options of `toggleterm`

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -3,6 +3,12 @@ local M = {}
 function M.preset()
   ---@class MiNVPresetTree
   local preset = {
+    setup = {
+      direction = "float",
+      float_opts = {
+        border = "single",
+      },
+    },
     keymaps = {
       --- Toggle terminal.
       toggle = "<C-t>",
@@ -13,18 +19,20 @@ end
 
 ---@param preset MiNVPresetTree
 function M.setup(preset)
-  local term = require("toggleterm")
+  local toggleterm = require("toggleterm")
   local utils = require("utils")
 
+  -- Setup toggleterm.
   local k_toggle = utils.register_key()
-  term.setup({ open_mapping = k_toggle })
+  toggleterm.setup(utils.tbl_merge(preset.setup, { open_mapping = k_toggle }))
+
+  -- Set keymaps.
   local keymaps = preset.keymaps
   utils.keymaps({
     noremap = false,
     { keymaps.toggle, k_toggle },
   })
-  -- Register a funtions to set keymaps.
-  local set_map = utils.register_fn(function()
+  utils.autocmd("TermOpen", "term://*toggleterm#*", function()
     local buf = vim.api.nvim_get_current_buf()
     utils.keymaps({
       mode = "t",
@@ -33,8 +41,6 @@ function M.setup(preset)
       { keymaps.toggle, k_toggle },
     })
   end)
-  local cmd = string.format([[lua require("utils").call_fn(%d)]], set_map)
-  utils.autocmd("TermOpen", "term://*toggleterm#*", cmd)
 end
 
 return M
